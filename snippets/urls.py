6b25a098372b70282d57a7a697d0d4ac6a5b0064
Juan Martinez <juandm1b1@gmail.com>
2020-11-03 15:54:30 -0500

    Primer commit tutorial REST

---
+++ snippets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()), # Vistas basadas en clases
+    path('snippets/<int:pk>', views.SnippetDetail.as_view()),
+    
+    #path('snippets/', views.snippet_list), # Vistas basadas en fumciones
+    #path('snippets/<int:pk>', views.snippet_detail),   
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
